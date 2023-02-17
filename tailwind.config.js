d9e9cd6eecda82086ccde800cd8a0ef98da7a712
henriquecfaguiar <henriquecfaguiar@gmail.com>
2023-02-17 20:59:49 -0300

    Add footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['Poppins', 'sans-serif']
+        'sans': ['Poppins', 'sans-serif'],
+        'JetBrainsMono': ['JetBrains Mono', 'sans-serif']
       }
     },
   },
